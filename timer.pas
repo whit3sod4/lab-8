2521d60d4c164832097123abab311b735f9c229c
whsd <whitesoda123@gmail.com>
2024-10-05 09:08:27 +0300

    Incredible changes and new program

---
+++ timer.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var h := ReadInteger('Введите количество часов:');
+  Assert ((h >= 0) and (h <= 23));
+  var m := ReadInteger('Введите количество минут:');
+  Assert ((m >= 0) and (m <= 59));
+  var s := ReadInteger('Введите количество секунд:');
+  Assert ((s >= 0) and (s <= 59));
+  print($'Общее количество секунд - {h * 3600 + m * 60 + s}');
+end.
